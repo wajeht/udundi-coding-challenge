fce3cd7f2cc6c3ae1068996b2ac387a8fd679e56
wajeht <zombyard@gmail.com>
2021-08-22 02:48:01 -0500

    files: add 404 page

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,26 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementsByClassName('card')[0].style.display = 'none';
-});
+/**
+ * The functions below will run as soon as DOM is loaded
+ */
+(function main() {
+    // Hide card at start
+    window.addEventListener('DOMContentLoaded', (event) => {
+        document.getElementsByClassName('card')[0].style.display = 'none';
+    });
 
-document.getElementById('more').addEventListener('click', () => {
-    document.getElementsByClassName('heading')[0].style.display = 'none';
-    document.querySelector('.card').classList.remove('animate__zoomOutUp');
-    document.querySelector('.card').classList.add('animate__zoomInUp');
-    document.getElementsByClassName('card')[0].style.display = '';
-});
+    // Hide heading and show card
+    document.getElementById('more').addEventListener('click', () => {
+        document.getElementsByClassName('heading')[0].style.display = 'none';
+        document.querySelector('.card').classList.remove('animate__zoomOutUp');
+        document.querySelector('.card').classList.add('animate__zoomInUp');
+        document.getElementsByClassName('card')[0].style.display = '';
+    });
 
-document.getElementById('close').addEventListener('click', () => {
-    document.querySelector('.card').classList.remove('animate__zoomInUp');
-    document.querySelector('.card').classList.add('animate__zoomOutUp');
-    document.getElementsByClassName('heading')[0].style.display = '';
-});
+    // Hide card and show heading
+    document.getElementById('close').addEventListener('click', () => {
+        document.querySelector('.card').classList.remove('animate__zoomInUp');
+        document.querySelector('.heading').classList.remove('animate__fadeInLeftBig');
+        document.querySelector('.heading').classList.add('animate__fadeIn');
+        document.querySelector('.card').classList.add('animate__zoomOutUp');
+        document.getElementsByClassName('heading')[0].style.display = '';
+    });
+})();
